f5099162e4ae0c622b60b63b5a94d57723a58aa5
thedarkscepter <60280897+thedarkscepter@users.noreply.github.com>
2021-10-19 16:02:00 +0530

    Add files via upload

---
+++ PRO-C158-Student-Activity-main/js/CursorEvents.js
@@ -0,0 +1,67 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" },
+  },
+  init: function () {
+    this.handleClickEvents();
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+    
+  },
+
+  handleClickEvents: function(){
+    this.el.addEventListener('click',evt => {
+      const placesContainer = document.querySelector('#places-container')
+      const{state} = placesContainer.getAttribute('tour') 
+      if (state === 'places-list'){
+        const id = this.el.getAttribute('id')
+        const placesId = [
+          'taj-mahal','budapest',"eiffel-tower","new-york-city"
+        ];
+        if(placesId.includes(id)){
+          placesContainer.setAttribute('tour', {
+            state: 'view',
+            selectedCard: id  
+          })
+        }
+      }
+    })
+  },
+
+  handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "orange",
+        opacity: 1,
+      });
+    }
+  },
+  handleMouseEnterEvents: function () {
+    // Mouse Enter Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", () =>  {
+      const{selectedItemId} = this.data
+      if (selectedItemId){
+        const el = document.querySelector(`#${selectedItemId}`)
+        const id = el.getAttribute('id')
+        if(id == selectedItemId){
+          el.setAttribute("material", {
+            color: "black",
+            opacity: 1,
+          });
+        }
+      } 
+    })
+  },
+});
